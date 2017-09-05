97ee9e69337de9fb1e87ca42f710fc0333538bec
azuesflip <azuesflip@gmail.com>
2017-09-05 16:54:26 -0600

    Added in new static pages with nothing on them. Set their routes to make more sense. Modified the tests to make sure they are performing properly. Also, made an adjustment to the blogs to get them to show in reverse to create a better user experience.

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get root" do
+    get '/'
+    assert_response :success
+  end
+
+  test "should get about" do
+    get '/about'
+    assert_response :success
+  end
+
+  test "should get contact" do
+    get '/about'
+    assert_response :success
+  end
+
+  test "should get help" do
+    get '/help'
+    assert_response :success
+  end
+
+end
