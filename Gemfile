e844fd1beea2ef49c486467052e68cc334464278
azuesflip <azuesflip@gmail.com>
2017-12-30 11:57:17 -0700

    Made some changes to the css to hopefully fix some resolution issues.

--- Gemfile
+++ Gemfile
@@ -33,6 +33,7 @@ gem 'bcrypt', '~> 3.1.11'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+gem 'jquery-rails'
 gem 'bootstrap-sass', '~> 3.3.7'
 gem 'rails-controller-testing'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'admins/new'
 
   root 'static_pages#home'
@@ -11,6 +13,11 @@ Rails.application.routes.draw do
 
   get  '/signup',  to: 'admins#new'
   post '/signup',  to: 'admins#create'
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
 
   resources :admins
   resources :shows
